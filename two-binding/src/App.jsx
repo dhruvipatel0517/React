aa4a498bd49ae892d4655c7f90a873fb6b287c92
djpatel052006 <shilpapt1984@gmail.com>
2026-03-17 12:32:18 +0530

    another 2 application created

---
+++ two-binding/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import LoginForm from './Components/loginform';
+
+
+
+const App = () => {
+  return (
+    <>
+      {/* two way binding: manage form filed using usestate </> */}
+      {/* {Login} */}
+    <LoginForm />
+
+    </>
+  );
+};
+
+export default App
